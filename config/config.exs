430bef63b7531c9de5d796ea48fcce79f0b2e968
1206santosh <santosh@linkwok.com>
2020-04-29 18:03:44 +0530

    captcha

--- config/config.exs
+++ config/config.exs
@@ -37,6 +37,10 @@ config :ex_aws,
   access_key_id: System.get_env("AWS_ACCESS_KEY"),
   secret_access_key: System.get_env("AWS_SECRET_KEY"),
   region: System.get_env("S3_REGION")
+
+config :recaptcha,
+    public_key: System.get_env("RECAPTCHA_PUBLIC_KEY"),
+    secret: System.get_env("RECAPTCHA_PRIVATE_KEY"),
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
